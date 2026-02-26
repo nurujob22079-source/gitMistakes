fa0758e202118275713264687f7eb2028353d2a8
Gulnour <nurujob22079@gmail.com>
2026-02-26 13:17:06 -0500

    Added changes method

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+ System.out.println("Hello, Worl  22222222d!");
+    }
+ public static void main(String[] args) {
+        System.out.println("Hello, World!");
+ 
+    }
+}
